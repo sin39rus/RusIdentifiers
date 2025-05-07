cf8584160f3322bf8b68553091a74ee11221a76b
Алексей Прошин <sin.06@mail.ru>
2025-05-07 10:01:35 +0200

    Добавлен ЕГАИС ФСРАР ID

---
+++ RusIdentifiers/Models/FSRAR/FsrarId.cs
@@ -0,0 +1,76 @@
+﻿using RusIdentifiers.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace RusIdentifiers.Models.FSRAR
+{
+    /// <summary>ФСРАР ID – уникальный идентификатор организации в ФСРАР.</summary>
+    public class FsrarId : IdentifierBase
+    {
+        private readonly string _value;
+
+        private FsrarId() { }
+
+        /// <summary>ФСРАР ID</summary>
+        /// <param name="value">Значение</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="RusIdentifiersArgumentException"></exception>
+        public FsrarId(string value)
+        {
+            value = value?.Trim();
+            if (IsValid(value))
+                _value = value;
+            else
+                throw new RusIdentifiersArgumentException($"Значение \"{value}\" не является номером ФСРАР ID.");
+        }
+
+        /// <summary>Проверка строки на значение ФСРАР ID</summary>
+        /// <param name="value">Строка</param>
+        /// <returns>true - если строка соответствует ФСРАР ID<br></br>false - если строка не соответствует ФСРАР ID</returns>
+        /// <exception cref="RusIdentifiersArgumentNullException"></exception>
+        public static bool IsValid(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new RusIdentifiersArgumentNullException(nameof(value));
+            if (value.Length != 12 || !long.TryParse(value, out _))
+                return false;
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public override XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        ///<summary>Преобразование значения в строку</summary>
+        public static implicit operator string(FsrarId value) => 
+            value.ToString();
+
+        ///<summary>Преобразование строки в значение</summary>
+        public static implicit operator FsrarId(string value) =>
+            new FsrarId(value);
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+}
---
+++ RusIdentifiersTests/Models/FsrarIdTests.cs
@@ -0,0 +1,57 @@
+﻿using RusIdentifiers.Exceptions;
+using RusIdentifiers.Models.FSRAR;
+
+namespace RusIdentifiersTests
+{
+    [TestClass()]
+    public class FsrarIdTests
+    {
+        [TestMethod]
+        public void CreateValidFsrarId()
+        {
+            _ = new FsrarId("000000000000");
+            _ = new FsrarId("020000024278");
+            _ = new FsrarId("020000031756");
+            _ = new FsrarId("020000034999");
+            _ = new FsrarId("020000713566");
+            _ = new FsrarId("030000736447");
+            _ = new FsrarId("030000736840");
+            _ = new FsrarId("030000737949");
+            _ = new FsrarId("030000739241");
+            _ = new FsrarId("030000739242");
+        }
+
+        [TestMethod]
+        public void CreateNotValidFsrarId()
+        {
+            Assert.ThrowsException<RusIdentifiersArgumentException>(() =>
+            {
+                _ = new FsrarId("02000002427");
+            });
+            Assert.ThrowsException<RusIdentifiersArgumentException>(() =>
+            {
+                _ = new FsrarId("0200000242723");
+            });
+            Assert.ThrowsException<RusIdentifiersArgumentException>(() =>
+            {
+                _ = new FsrarId("A02000002422");
+            });
+
+            Assert.ThrowsException<RusIdentifiersArgumentNullException>(() =>
+            {
+                _ = new FsrarId("");
+            });
+            Assert.ThrowsException<RusIdentifiersArgumentNullException>(() =>
+            {
+                _ = new FsrarId(null);
+            });
+        }
+
+        [TestMethod]
+        public void ImplicitTests()
+        {
+            FsrarId id = "020000031756";
+            Assert.AreEqual("020000031756", id.ToString());
+        }
+    }
+}
